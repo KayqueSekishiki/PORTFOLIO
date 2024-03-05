900f297da5eef54082c04a69af48da5722c776fa
Kayque Lopes <104032451+KayqueSekishiki@users.noreply.github.com>
2024-03-05 02:57:29 -0300

    Add Avatar Border Animation and Remove Modal CV!

--- assets/js/cvTrigger.js
+++
@@ -1,6 +0,0 @@
-const cvTrigger = document.getElementById("profile.cv--trigger");
-const cvModal = document.getElementById("profile.cv--modal");
-
-cvTrigger.addEventListener("click", () => {
-  cvModal.style.display = "flex";
-});
