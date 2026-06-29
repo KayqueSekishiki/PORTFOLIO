8112d644d912443860eed1fbbf7cb0e80a0a5407
Kayque Lopes <104032451+KayqueSekishiki@users.noreply.github.com>
2026-06-29 19:02:55 -0300

    feat: hero

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -1,11 +1,13 @@
 import ArrowToTop from "@/components/ArrowToTop/ArrowToTop";
 import styles from "./main.module.scss";
 import Header from "@/components/Header/Header";
+import Hero from "@/components/Hero/Hero";
 
 export default function Main() {
   return (
     <div>
       <Header />
+      <Hero />
       <ArrowToTop />
       <main className={styles.main}></main>
     </div>
---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,46 @@
+import Link from "next/link";
+import styles from "./Hero.module.scss";
+import { LucideTerminal } from "lucide-react";
+
+export default function Hero() {
+  return (
+    <section className={styles.hero}>
+      <div className={styles.content}>
+        <span className={styles.subtitle}>npm install curriculum-vitae</span>
+
+        <h2>
+          Olá, eu sou
+          <span>Kayque Sekishiki</span>
+          <small>
+            Seu próximo <strong>Desenvolvedor Frontend</strong>
+          </small>
+        </h2>
+
+        <p>
+          Especialista em criar interfaces de alta performance, fundindo design
+          com tecnologias modernas.
+        </p>
+
+        <div className={styles.buttons}>
+          <Link href="#projects">Ver Projetos</Link>
+
+          <Link href="#contact">Contato</Link>
+        </div>
+      </div>
+
+      <div className={styles.animation}>
+        <div className={styles.square1} />
+        <div className={styles.square2} />
+        <div className={styles.square3} />
+        <div className={styles.icon}>
+          <LucideTerminal size={120} strokeWidth={5} />
+        </div>
+      </div>
+
+      <div className={styles.scroll}>
+        <span>SCROLL_DOWN</span>
+        <div className={styles.scrollLine}></div>
+      </div>
+    </section>
+  );
+}
